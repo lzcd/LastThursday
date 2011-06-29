34d8f8469976c3348540d00666d7b7247e6212b6
lzcd <lzcd@hotmail.com>
2011-06-29 16:12:42 +1000

    Added solution

---
+++ LastThursday/Timezones.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LastThursday
+{
+    public class Timezones
+    {
+
+        public static IEnumerable<TimeZoneInfo> GeneratePossibleMatches(int hour, int minute, DateTime serverUtcNow, int errorMarginMinutes = 14)
+        {
+            var errorSpan = new TimeSpan(0,errorMarginMinutes,0);
+            var alternateHour = (hour + 12) % 24;
+
+            return from timezone in TimeZoneInfo.GetSystemTimeZones()
+                      let timezoneTime = serverUtcNow.Add(timezone.BaseUtcOffset)
+                      let minimumTime = timezoneTime.Subtract(errorSpan)
+                      let maximumTime = timezoneTime.Add(errorSpan)
+                      where (hour >= minimumTime.Hour &&
+                            minute >= minimumTime.Minute &&
+                            hour <= maximumTime.Hour &&
+                            minute <= maximumTime.Minute) ||
+                            (alternateHour >= minimumTime.Hour &&
+                            minute >= minimumTime.Minute &&
+                            alternateHour <= maximumTime.Hour &&
+                            minute <= maximumTime.Minute)
+                      select timezone;
+ 
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LastThursday;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var timezones = TimeZoneInfo.GetSystemTimeZones();
+            var serverNow = new DateTime(2011, 5, 24, 21, 45, 0);
+            var serverTimezone = TimeZoneInfo.FindSystemTimeZoneById("AUS Eastern Standard Time");
+            var serverUtcNow = serverNow.Subtract(serverTimezone.BaseUtcOffset);
+          
+            var possiblities = Timezones.GeneratePossibleMatches(9, 43, serverUtcNow);
+        }
+    }
+}
